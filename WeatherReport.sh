f2fceda4e509350998cc5c07178667a6fe4cd66d
Paulin GAILLOT <paulin.gaillot@isen-ouest.yncrea.fr>
2022-05-03 16:32:47 +0200

    Weather Report

--- WeatherReport.sh
+++ WeatherReport.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+for( i=0; i < 24; i++ );  do
+    line=`wc -l ./main/main_${i}.txt | awk 'print $1  }'`
+    tot=0
+    tab=`echo "./temp/temp_${i}.txt"`
+    for( j=0; j < 24; j++ );  do
+            tot = tot + tab[j]
+    done
+    value=tot/line
+done
+
+
+line=`wc -l ./temp/temp_16.txt | awk 'print $1  }'`
+for( i=0; i < 24; i++ );  do
+
+done
+
+line=`wc -l ./humidite/humidite_16.txt | awk 'print $1  }'`
+for( i=0; i < 24; i++ );  do
+
+done
